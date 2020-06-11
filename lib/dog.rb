4270f94846906983d00f5050ba15b76b00c36bec
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-11 16:35:32 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+	class Dog
+	  def initialize(name, breed)
+    @name = name
+    @breed = breed
+		end
+
+		def name
+			@name
+
+	  def breed
+	    @breed
+
+  		end
+		end
+		fido = Dog.new("fido","Pug")
+		fido.name = fido
+		fido.breed = pug
+	end
+
+
+	if fido = fido.breed
+	else
+		return "mutt"
+	end
